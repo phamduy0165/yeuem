6bd371f749cb7cabc5deb3f4aec8e3dba2313ae1
phamduy0165 <phamvanduy147@gmail.com>
2025-05-21 08:57:32 +0700

    Lần đầu đẩy code lên GitHub

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Moshi Moshi!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    mà sao em cứ đi nhầm đường
+    lạc vào tim anh lẻ loi
+    đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 10h anh call nhé công chúa.',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, em trai à :3',
+    question:'Trên thế giới hơn 9 tỉ người mà sao thích em <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Thích thì thích mà không thích thì thích <33333333',
+    mess: 'Em biết mà 🥰. Tối đi chơi chị nhé 😘😘',
+    messDesc: 'Tối nay 7h call em nhé chị đẹp .',
+    messLink: 'https://www.facebook.com/phamvan.duy.92102'
    
 }
