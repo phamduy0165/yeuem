6da8b29486c37ad1a683b8cb5384ed4c90bbe57e
phamduy0165 <phamvanduy147@gmail.com>
2025-08-08 13:43:56 +0700

    2uynh

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Mở file config.js sửa nội dung theo mẫu
 ```
 const CONFIG = {
-    introTitle: 'em 2uỳnh à!',
+    introTitle: 'em  à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
     introTitle: 'Moshi Moshi!',
-    introDesc: `Đây chỉ là một lời mời cute từ một dân IT
-    được em làm khác đi một chút,
-    hông phiền click tiếp nha chị đẹp.
-    Chị Én nhà em dạo này từ hôm đi về bụng yếu khó mời đi chơi lắm`,
+    introDesc: `Đây chỉ là một trò vui vẻ làm ra lúc làm việc căng thẳng từ một dân IT,
+    hông phiền click tiếp nha ngườiđẹp.
+    Xem xong có feedback gì thì báo lại ngay cho người làm nhé`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích em <33',
-    btnNo: 'Không, em trai à :3',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, anh trai à :3',
     question:'Trên thế giới hơn 9 tỉ người mà sao thích em <3',
-    btnReply: 'Gửi cho em <3',
+    btnReply: 'Gửi cho anh <3',
     reply: 'Thích thì thích mà không thích thì thích <33333333',
-    mess: 'Em biết mà 🥰. Tối nếu không bị thì phản hồi cho em nha 😳😳',
-    messDesc: 'Tối nay 8h call em nhé chị đẹp .',
+    mess: 'Anh biết mà 🥰. Tối nếu không bị thì phản hồi cho anh nha 😳😳',
+    messDesc: 'Tối nay nhắn mấy giờ cũng được nha người đẹp .',
     messLink: 'https://www.facebook.com/phamvan.duy.92102'
    
 }
